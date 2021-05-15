810ad5d17af0b13e94860e04b2c8a27decfff420
SmallQ008 <71476570+SmallQ008@users.noreply.github.com>
2021-05-16 07:48:54 +0800

    123

--- HWH_2D_Final_0608420/Assets/腳本/player1.cs
+++ HWH_2D_Final_0608420/Assets/腳本/player1.cs
@@ -3,5 +3,46 @@ using UnityEngine;
 
 public class player1 : MonoBehaviour
 {
+    [Header("等級")]
+    [Tooltip("這是角色的等級")]
+    public int lv = 1;
+    [Header("移動速度"),Range(0,300)]
+    public float speed = 10.5f;
+    public bool isDead = false;
+    [Tooltip("這是角色的名稱")]
+    public string cName = "貓咪";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+
+
+
+    private void Move()
+    {
+        print("移動");
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void Hit()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+    private void Start()
+    {
+        Move();
+    }
+    private void Update()
+    {
+        Move();
+    }
+
 
 }
